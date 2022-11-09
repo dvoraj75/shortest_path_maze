8ea7b5934367af33975dc6e8a4e8898e40f6104d
Jan Dvořák <dvoraj75@gmail.com>
2022-11-09 06:54:59 +0100

    Add maze class

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ from collections import deque
 from typing import NamedTuple, List, IO, Tuple
 
 
+def read_maze(maze_file: IO[str]) -> List[List[str]]:
+    """
+    Read file with the maze and covert it to 2d array
+    """
+    return [[c for c in line if c != "\n"] for line in maze_file]
+
+
 class Point(NamedTuple):
     """
     Simple class representing point in maze
@@ -26,36 +33,77 @@ class Point(NamedTuple):
     y: int
 
 
-def read_maze(maze_file: IO[str]) -> List[List[str]]:
+class Maze:
     """
-    Read file with the maze and covert it to 2d array
+    Class representing 2d maze
     """
-    return [[c for c in line if c != "\n"] for line in maze_file]
 
+    def __init__(
+        self,
+        maze: List[List[str]],
+        start_point_char: str = "0",
+        end_point_char: str = "F",
+        wall: str = "#",
+        free_space: str = " ",
+    ):
+        self.maze: List[List[str]] = maze
+        self.start_point_char: str = start_point_char
+        self.end_point_char: str = end_point_char
+        self.wall: str = wall
+        self.free_space: str = free_space
+        self.start_point: Point = self.find_point(self.start_point_char)
+        self.end_point: Point = self.find_point(self.end_point_char)
+        self.x_size = len(self.maze[0])
+        self.y_size = len(self.maze)
 
-def print_maze(maze: List[List[str]]) -> None:
-    """
-    Prin maze 2d array
-    """
-    for line in maze:
-        for char in line:
-            if char in ("-", "|", "0", "F", "#"):
-                print(char, end="")
-            else:
-                print(" ", end="")
-        print()
+        self.check_maze()
 
+    def check_maze(self):
+        """
+        Check if maze is rectangular
+        """
+        first_line = len(self.maze[0])
+        for line in self.maze:
+            if len(line) != first_line:
+                raise ValueError("Maze is not rectangle!")
 
-def find_point(maze: List[List[str]], char: str) -> Point:
-    """
-    Find character 'char' in the maze. Using for finding start and end point
-    """
-    for y_coord, line in enumerate(maze):
-        try:
-            return Point(line.index(char), y_coord)
-        except ValueError:
-            pass
-    raise ValueError(f"Maze doesnt have point with '{char}' character")
+    def print_maze(self):
+        """
+        Print maze 2d array
+        """
+        for line in self.maze:
+            for char in line:
+                print(
+                    char
+                    if char
+                    in ("-", "|", self.start_point_char, self.end_point_char, self.wall)
+                    else " ",
+                    end="",
+                )
+            print()
+
+    def find_point(self, char: str) -> Point:
+        """
+        Find character 'char' in the maze. Using for finding start and end point
+        """
+        for y_coord, line in enumerate(self.maze):
+            try:
+                return Point(line.index(char), y_coord)
+            except ValueError:
+                pass
+        raise ValueError(f"Maze doesn't have point with '{char}' character")
+
+    def get_value(self, x: int, y: int) -> str:
+        """
+        Get value from x, y coords
+        """
+        return self.maze[y][x]
+
+    def set_value(self, x: int, y: int, value: str):
+        """
+        Set value to x, y coords
+        """
+        self.maze[y][x] = value
 
 
 class BFSMazeSolver:
@@ -71,19 +119,9 @@ class BFSMazeSolver:
 
     def __init__(
         self,
-        maze: List[List[str]],
-        star_point_char: str = "0",
-        end_point_char: str = "F",
-        wall: str = "#",
-        free_space: str = " ",
+        maze: Maze,
     ):
-        self.maze: List[List[str]] = maze
-        self.start_point: Point = find_point(self.maze, star_point_char)
-        self.end_point: Point = find_point(self.maze, end_point_char)
-        self._start_point_char: str = star_point_char
-        self._end_point_char: str = end_point_char
-        self._wall: str = wall
-        self._free_space: str = free_space
+        self.maze: Maze = maze
         self.step_count: int = 0
 
     def solve(self):
@@ -91,15 +129,18 @@ class BFSMazeSolver:
         Solve the maze
         """
         points = deque()
-        points.append(self.start_point)
+        points.append(self.maze.start_point)
 
         while points:
             current_point = points.popleft()
-            if current_point == self.end_point:
+            if current_point == self.maze.end_point:
                 break
             points.extend(self.make_steps(current_point))
 
-        if maze[self.end_point.y][self.end_point.x] == self._end_point_char:
+        if (
+            self.maze.get_value(self.maze.end_point.x, self.maze.end_point.y)
+            == self.maze.end_point_char
+        ):
             raise ValueError("Maze doesn't have solution!")
 
         self.find_shortest_path()
@@ -108,59 +149,65 @@ class BFSMazeSolver:
         """
         Look arround and make steps in four directions
         """
-        current_step = int(maze[current_point.y][current_point.x])
+        current_step = int(self.maze.get_value(current_point.x, current_point.y))
         next_points = []
 
         for step in self.STEPS:
-            if self.can_make_step(
-                current_point, current_point.x + step.x, current_point.y + step.y
-            ):
-                self.maze[current_point.y + step.y][current_point.x + step.x] = (
-                    current_step + 1
+            if self.can_make_step(current_point.x + step.x, current_point.y + step.y):
+                self.maze.set_value(
+                    current_point.x + step.x,
+                    current_point.y + step.y,
+                    str(current_step + 1),
                 )
                 next_points.append(
                     Point(current_point.x + step.x, current_point.y + step.y)
                 )
-
         return next_points
 
-    def can_make_step(self, current_point: Point, x: int, y: int) -> bool:
+    def can_make_step(self, x: int, y: int) -> bool:
         """
         Check if can make step
         """
-        return self.is_safe(current_point, x, y) and (
-            self.maze[y][x] == self._free_space or self.maze[y][x] == self._end_point_char
+        return self.is_safe(x, y) and (
+            self.maze.get_value(x, y) == self.maze.free_space
+            or self.maze.get_value(x, y) == self.maze.end_point_char
         )
 
-    def is_safe(self, current_point: Point, x: int, y: int) -> bool:
+    def is_safe(self, x: int, y: int) -> bool:
         """
         Check if index is not out of the range
         """
-        return 0 <= x < len(self.maze[current_point.y]) and 0 <= y < len(self.maze)
+        return 0 <= x < self.maze.x_size and 0 <= y < self.maze.y_size
 
     def find_shortest_path(self):
         """
         Go back to the start and count the steps
         """
-        current_point = self.end_point
-        current_step = int(self.maze[self.end_point.y][self.end_point.x])
+        current_point = self.maze.end_point
+        current_step = int(
+            self.maze.get_value(self.maze.end_point.x, self.maze.end_point.y)
+        )
         while current_step:
             for step in self.STEPS:
                 if (
                     self.is_safe(
-                        current_point,
                         current_point.x + step.x,
                         current_point.y + step.y,
                     )
-                    and isinstance(
-                        self.maze[current_point.y + step.y][current_point.x + step.x],
-                        int,
+                    and self.maze.get_value(
+                        current_point.x + step.x, current_point.y + step.y
+                    ).isdigit()
+                    and int(
+                        self.maze.get_value(
+                            current_point.x + step.x, current_point.y + step.y
+                        )
                     )
-                    and self.maze[current_point.y + step.y][current_point.x + step.x]
                     == current_step - 1
                 ):
-                    maze[current_point.y + step.y][current_point.x + step.x] = (
-                        "|" if step.y else "-"
+                    self.maze.set_value(
+                        current_point.x + step.x,
+                        current_point.y + step.y,
+                        "|" if step.y else "-",
                     )
                     current_point = Point(
                         current_point.x + step.x, current_point.y + step.y
@@ -168,31 +215,31 @@ class BFSMazeSolver:
                     break
             self.step_count += 1
             current_step -= 1
-        self.maze[self.end_point.y][self.end_point.x] = self._end_point_char
+        # reset start/end point
+        self.maze.set_value(
+            self.maze.start_point.x, self.maze.start_point.y, self.maze.start_point_char
+        )
+        self.maze.set_value(
+            self.maze.end_point.x, self.maze.end_point.y, self.maze.end_point_char
+        )
 
-    def print_solution(self, _print_maze: bool = False) -> None:
+    def print_solution(self, _print_maze: bool = False):
         """
         Print final solution
         """
         print(f"SOLUTION:\nStep count: {self.step_count}\nused method: bfs")
         if _print_maze:
-            print_maze(self.maze)
+            self.maze.print_maze()
 
 
 if __name__ == "__main__":
-    """
-    TODO A ZLEPSENI: maze by asi mohla byt vlastni classa, protoze veci jako endpoit_char,
-    start_point_char,..etc nalezi spise bludisti nez solveru. Lepe by se pak pracovalo i 
-    s velikosti bludiste. Daly by se napsat i checky, ze bludiste je opravdu obdelnik atd... 
-    """
     parser = argparse.ArgumentParser(
         prog="MazeSolver", description="Program finds the shortest solution for maze"
     )
     parser.add_argument("maze_file", type=argparse.FileType("r"))
     parser.add_argument("-p", "--print-maze", action="store_true", dest="print_maze")
     args = parser.parse_args()
-    maze = read_maze(args.maze_file)
 
-    solver = BFSMazeSolver(maze)
+    solver = BFSMazeSolver(Maze(read_maze(args.maze_file)))
     solver.solve()
     solver.print_solution(args.print_maze)
